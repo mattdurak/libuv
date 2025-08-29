f26b9879f49d5773d853a196e22c9a1ee6d9fd5c
Matt Durak <mdurak@microsoft.com>
2025-08-29 15:25:17 -0700

    Fix buffer overrun due to _snwprintf_s arguments in process.c
    See [_snwprintf_s](https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/snprintf-s-snprintf-s-l-snwprintf-s-snwprintf-s-l?view=msvc-170).
    
    > sizeOfBuffer
    > The size of the storage location for output. Size in bytes for the functions that take char, **and words for those that take wchar_t**.
    
    The buffer size should be the count of characters but it is currently a count of bytes.

--- src/win/process.c
+++ src/win/process.c
@@ -1220,7 +1220,7 @@ static int uv__kill(HANDLE process_handle, int signum) {
                              NULL,
                              &localappdata);
         _snwprintf_s(dump_folder,
-                     sizeof(dump_folder),
+                     sizeof(dump_folder)/sizeof(dump_folder[0]),
                      _TRUNCATE,
                      L"%ls\\CrashDumps",
                      localappdata);
@@ -1233,7 +1233,7 @@ static int uv__kill(HANDLE process_handle, int signum) {
 
       /* Construct dump filename from process name and PID. */
       _snwprintf_s(dump_name,
-                   sizeof(dump_name),
+                   sizeof(dump_name)/sizeof(dump_name[0]),
                    _TRUNCATE,
                    L"%ls\\%ls.%d.dmp",
                    dump_folder,
